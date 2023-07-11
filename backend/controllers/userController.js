7fc73f58674e6b2ba87a0d9cbaafc7a4b83b4ac2
Muhammad Aqib <aqibafridi0345@gmail.com>
2023-07-11 14:24:20 +0500

    model and db connection

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,33 @@ import asyncHandler from "express-async-handler";
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  //   const { email, password } = req.body;
-
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {};
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
