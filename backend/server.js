057eaceca5088635d6f8ec9b1f40ea90e081516d
Muhammad Aqib <aqibafridi0345@gmail.com>
2023-07-11 12:20:04 +0500

    controler and api testing

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
-
-app.listen(port, () => `server started on port ${port}`);
+app.use("/api/users", userRoutes);
+app.listen(port, () => console.log(`server started on port ${port}`));
