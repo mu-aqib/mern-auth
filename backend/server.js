2a3ff8eb1577cc7a6c4363dd9c5f8bb7f83c7c29
Muhammad Aqib <aqibafridi0345@gmail.com>
2023-07-11 11:37:46 +0500

    user Router and user controller

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.listen(port, () => `server started on port ${port}`);
